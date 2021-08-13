7b334236e63a3b1f58a2fe76aef590bbf4224f9a
Aditya <60613717+Aditya7feb@users.noreply.github.com>
2021-08-13 11:59:35 +0530

    multipe js removed

--- assets/vendor/venobox/venobox.js
+++ assets/vendor/venobox/venobox.js
@@ -57,7 +57,7 @@
                 titleBackground: '#161617',
                 titleColor: '#d2d2d2',
                 titlePosition : 'top', // 'top' || 'bottom'
-                share: [], // ['facebook', 'twitter', 'linkedin', 'pinterest', 'download']
+                //share: [], // ['facebook', 'twitter', 'linkedin', 'pinterest', 'download']
                 cb_pre_open: function(){ return true; }, // Callbacks - thanx @garyee
                 cb_post_open: function(){},
                 cb_pre_close: function(){ return true; },
@@ -339,88 +339,88 @@
                 }); // click
 
                 /* -------- CHECK NEXT / PREV -------- */
-                function checknav(){
+                // function checknav(){
 
-                    thisgall = obj.data('gall');
-                    numeratio = obj.data('numeratio');
-                    gallItems = obj.data('gallItems');
-                    infinigall = obj.data('infinigall');
-                    share = obj.data('share');
-                    blockshare.html('');
-                    if ( obj.data('vbtype') !== 'iframe' && obj.data('vbtype') !== 'inline' && obj.data('vbtype') !== 'ajax' ) {
-                        sharelinks = { 
-                            pinterest : '<a target="_blank" href="https://pinterest.com/pin/create/button/?url='+obj.prop('href')+'&media='+obj.prop('href')+'&description='+title+'">'+pinIcon+'</a>', 
-                            facebook  : '<a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u='+obj.prop('href')+'">'+fbIcon+'</a>', 
-                            twitter   : '<a target="_blank" href="https://twitter.com/intent/tweet?text='+title+'&url='+obj.prop('href')+'">'+twitterIcon+'</a>', 
-                            linkedin  : '<a target="_blank" href="https://www.linkedin.com/sharing/share-offsite/?url='+obj.prop('href')+'">'+linkedinIcon+'</a>',
-                            download  : '<a target="_blank" href="'+obj.prop('href')+'">'+downloadIcon+'</a>'
-                        };
-                        $.each( share, function( key, value ) {
-                            blockshare.append(sharelinks[value]);
-                        });
-                    }
+                //     thisgall = obj.data('gall');
+                //     numeratio = obj.data('numeratio');
+                //     gallItems = obj.data('gallItems');
+                //     infinigall = obj.data('infinigall');
+                //     share = obj.data('share');
+                //     blockshare.html('');
+                //     if ( obj.data('vbtype') !== 'iframe' && obj.data('vbtype') !== 'inline' && obj.data('vbtype') !== 'ajax' ) {
+                //         sharelinks = { 
+                //             pinterest : '<a target="_blank" href="https://pinterest.com/pin/create/button/?url='+obj.prop('href')+'&media='+obj.prop('href')+'&description='+title+'">'+pinIcon+'</a>', 
+                //             //facebook  : '<a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u='+obj.prop('href')+'">'+fbIcon+'</a>', 
+                //             twitter   : '<a target="_blank" href="https://twitter.com/intent/tweet?text='+title+'&url='+obj.prop('href')+'">'+twitterIcon+'</a>', 
+                //             linkedin  : '<a target="_blank" href="https://www.linkedin.com/sharing/share-offsite/?url='+obj.prop('href')+'">'+linkedinIcon+'</a>',
+                //             download  : '<a target="_blank" href="'+obj.prop('href')+'">'+downloadIcon+'</a>'
+                //         };
+                //         $.each( share, function( key, value ) {
+                //             blockshare.append(sharelinks[value]);
+                //         });
+                //     }
 
-                    if (gallItems) {
-                        items = gallItems;
-                    } else {
-                        items = $('.vbox-item[data-gall="' + thisgall + '"]');
-                    }
+                //     if (gallItems) {
+                //         items = gallItems;
+                //     } else {
+                //         items = $('.vbox-item[data-gall="' + thisgall + '"]');
+                //     }
 
-                    if (items.length < 2) {
-                        infinigall = false;
-                        numeratio = false;
-                    }
+                //     if (items.length < 2) {
+                //         infinigall = false;
+                //         numeratio = false;
+                //     }
 
-                    thenext = items.eq( items.index(obj) + 1 );
-                    theprev = items.eq( items.index(obj) - 1 );
+                //     thenext = items.eq( items.index(obj) + 1 );
+                //     theprev = items.eq( items.index(obj) - 1 );
 
-                    if (!thenext.length && infinigall === true) {
-                      thenext = items.eq(0);
-                    }
+                //     if (!thenext.length && infinigall === true) {
+                //       thenext = items.eq(0);
+                //     }
 
-                    // update gall numeration
-                    if (items.length >= 1) {
-                      gallIndex = items.index(obj)+1;
-                      blocknum.html(gallIndex + ' / ' + items.length);
-                    } else {
-                      gallIndex = 1;
-                    }
-                    if (numeratio === true) {
-                      blocknum.show();
-                    } else {
-                      blocknum.hide();
-                    }
+                //     // update gall numeration
+                //     if (items.length >= 1) {
+                //       gallIndex = items.index(obj)+1;
+                //       blocknum.html(gallIndex + ' / ' + items.length);
+                //     } else {
+                //       gallIndex = 1;
+                //     }
+                //     if (numeratio === true) {
+                //       blocknum.show();
+                //     } else {
+                //       blocknum.hide();
+                //     }
 
-                    // update title
-                    if (title !== '') {
-                      blocktitle.show();
-                    } else {
-                      blocktitle.hide();
-                    }
+                //     // update title
+                //     if (title !== '') {
+                //       blocktitle.show();
+                //     } else {
+                //       blocktitle.hide();
+                //     }
 
-                    // update navigation arrows
-                    if (!thenext.length && infinigall !== true) {
-                      $('.vbox-next').css('display', 'none');
-                      nextok = false;
-                    } else {
-                      $('.vbox-next').css('display', 'block');
-                      nextok = true;
-                    }
+                //     // update navigation arrows
+                //     if (!thenext.length && infinigall !== true) {
+                //       $('.vbox-next').css('display', 'none');
+                //       nextok = false;
+                //     } else {
+                //       $('.vbox-next').css('display', 'block');
+                //       nextok = true;
+                //     }
 
-                    if (items.index(obj) > 0 || infinigall === true) {
-                      $('.vbox-prev').css('display', 'block');
-                      prevok = true;
-                    } else {
-                      $('.vbox-prev').css('display', 'none');
-                      prevok = false;
-                    }
-                    // activate swipe
-                    if (prevok === true || nextok === true) {
-                      content.on(TouchMouseEvent.DOWN, onDownEvent);
-                      content.on(TouchMouseEvent.MOVE, onMoveEvent);
-                      content.on(TouchMouseEvent.UP, onUpEvent);
-                    }
-                }
+                //     if (items.index(obj) > 0 || infinigall === true) {
+                //       $('.vbox-prev').css('display', 'block');
+                //       prevok = true;
+                //     } else {
+                //       $('.vbox-prev').css('display', 'none');
+                //       prevok = false;
+                //     }
+                //     // activate swipe
+                //     if (prevok === true || nextok === true) {
+                //       content.on(TouchMouseEvent.DOWN, onDownEvent);
+                //       content.on(TouchMouseEvent.MOVE, onMoveEvent);
+                //       content.on(TouchMouseEvent.UP, onUpEvent);
+                //     }
+                // }
 
                 /* -------- gallery navigation -------- */
                 function navigateGall(destination) {
